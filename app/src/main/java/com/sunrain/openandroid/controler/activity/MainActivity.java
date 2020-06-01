4ec91d430f1d71807e9a5f6e5050edf54b990c9b
jinglei <965334541@qq.com>
2020-06-01 14:02:26 +0800

    我的第一次更新项目（测试）

---
+++ app/src/main/java/com/sunrain/openandroid/controler/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sunrain.openandroid.controler.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import com.sunrain.openandroid.R;
+
+public class  MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        MyView myView = new MyView(this);
+//        myView.setExampleString("测试");
+        setContentView(R.layout.sample_my_view);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    }
+}
